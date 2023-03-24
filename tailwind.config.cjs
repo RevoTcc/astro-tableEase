42659c9657d204bbad2b449280f17ed8ceaf5cfb
kiwizitos <matpinheiro03@gmail.com>
2023-03-24 11:32:33 -0300

    adding colors on tailwind file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black0: '#191919',
+        black50: '#1E1E1E',
+        black100: '#333333',
+        gray: '#969696',
+        white: '#FAFAFA',
+
+        te_red: '#FF5A5F',
+        te_yellow: '#FFC947',
+        te_blue: '#00BFFF',
+        te_blueAccent: '#6EC4E8',
+        te_gray: '#CFCFCF',
+      },
+    },
+  },
+  plugins: [],
 }
